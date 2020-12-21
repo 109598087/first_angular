54e27084d308add62c50a98706a4f288d3acf95d
109598087 <t109598087@ntut.org.tw>
2020-12-21 11:10:18 +0800

    first_page

--- src/app/choose-project-page/choose-project-page.component.ts
+++ src/app/choose-project-page/choose-project-page.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./choose-project-page.component.css']
 })
 export class ChooseProjectPageComponent implements OnInit {
-
+  name = 'John Cena';
+  conpanyName = 'WWE';
+  jobTitle = 'Wrestler';
+  location = 'USA';
+  selfIntroduction = '我是大帥哥';
   constructor() { }
 
   ngOnInit(): void {
